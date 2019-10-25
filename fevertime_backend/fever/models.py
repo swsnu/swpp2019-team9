23aeb36e28036efb10068e79cd2d68e42d395303
YooYoungJae <dudwo235@snu.ac.kr>
2019-10-25 18:43:31 +0900

    10/25(금) YooYoungJae fevertime 폴더를 fevertime_backend 라고 만든뒤 fevertime 으로 폴더명만 바꾼듯한 흔적이 보여 settings.py 와 manage.py 를 조금 바꿈

--- fevertime_backend/fever/models.py
+++ fevertime_backend/fever/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Fever_history(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='fever_history_user'
+    )
+    start_time = models.DateTimeField(auto_now_add=True)
+    end_time = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=45)
+    total_time = models.DateField()
+    fever_time = models.DateField()
+    fever_rate = models.DateField()
+    fever_count = models.IntegerField(default=0)
+    click_end = models.CharField(max_length=1, default='N')
+
+
+class Fever_progress(models.Model):
+    reg_date = models.DateTimeField(auto_now_add=True)
+    fever_yn = models.CharField(max_length=1, default='N')
+    fever_history = models.ForeignKey(
+        Fever_history,
+        on_delete=models.CASCADE,
+        related_name='hid'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='fever_progress_user'
+    )
--- fevertime_backend/fevertime/settings.py
+++ fevertime_backend/fevertime/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fever.apps.FeverConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'fevertime_backend.urls'
+ROOT_URLCONF = 'fevertime.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'fevertime_backend.wsgi.application'
+WSGI_APPLICATION = 'fevertime.wsgi.application'
 
 
 # Database
