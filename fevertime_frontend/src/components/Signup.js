354f0fa01479c0870d77ceb509b44252d979bf2b
Lee Seung Yul <syulee0124@gmail.com>
2019-10-25 17:55:29 +0900

    react part of SignUp is added with test code

--- fevertime_frontend/src/components/Signup.js
+++ fevertime_frontend/src/components/Signup.js
@@ -1,6 +1,86 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+
+import PopUpModal from "./component/PopUpModal"
+/* 
+    need /api/signup/ 
+        get(ID)=> boolean :  check if ID is okay to use
+        post(ID, Nickname, Password) : make new user
+*/
 class Signup extends Component {
+    state = {
+        ID : "",
+        Nickname : "",
+        Password : "",
+        Password_Confirm : "",
+        Term_of_Use : false,
+        Term_of_Personal_info : false,
+        WrongInput : ["","","","","",""],
+    }
+
+    clickSignUp = () =>{
+        let LocalWrongInput = ["","","","","",""]
+        let wrong = false
+        let result
+        if(this.state.ID !== ""){
+            result=axios.get('/api/signup/', this.state.ID)
+                    .then(res => {return res})
+                    .catch(error => {return error.response})
+            if(result.status === 200){
+                if(!result.data){
+                    LocalWrongInput.splice(0,1, "ID Exist")
+                    wrong = true
+                }
+            }
+            else{
+                LocalWrongInput.splice(0,1, "Server Not responding")
+                wrong = true
+            }           
+
+        }
+        else{
+            LocalWrongInput.splice(0,1, "Empty ID")
+            wrong = true
+        }
+
+        if(this.state.Nickname === ""){
+            LocalWrongInput.splice(1,1, "Empty Nickname")
+            wrong = true
+        }
+
+        if(this.state.Password === ""){
+            LocalWrongInput.splice(2,1,"Empty Password")
+            wrong = true
+        }
+
+        if(this.state.Password !== this.state.Password_Confirm){
+            LocalWrongInput.splice(3,1,"Wrong Password Confirm")
+            wrong = true
+        }
+
+        if(!this.state.Term_of_Use){
+            LocalWrongInput.splice(4,1,"Please Check")
+            wrong = true
+        }
+
+        if(!this.state.Term_of_Personal_info){
+            LocalWrongInput.splice(5,1,"Please Check")
+            wrong = true
+        }
+
+        this.setState({WrongInput : LocalWrongInput})
+
+        if(!wrong){
+            axios.post('/api/signup/', {
+                                        ID : this.state.ID,
+                                        Nickname : this.state.Nickname,
+                                        Password : this.state.Password,})
+            .then(res => {})
+            this.props.history.push('/')
+        }
+    }
+
     render() {
         return (
             <div className='form-container Signup'>
@@ -8,36 +88,69 @@ class Signup extends Component {
                 <div className='d-flex mt-5 d-v-center'>
                     <div className='w-20'></div>
                     <div className='w-20'>ID</div>
-                    <input className='w-30 input-1'/>
+                    <input className='w-30 input-1'
+                            id="ID_input"
+                            onChange={(event => this.setState({
+                                ID: event.target.value}))}
+                    />
+                    {this.state.WrongInput[0]}
                 </div>
                 <div className='d-flex mt-3 d-v-center'>
                     <div className='w-20'></div>
                     <div className='w-20'>Nickname</div>
-                    <input className='w-30 input-1'/>
+                    <input className='w-30 input-1'
+                            id="Nickname_input"
+                        onChange={(event => this.setState({
+                            Nickname: event.target.value}))}
+                    />
+                    {this.state.WrongInput[1]}
                 </div>
                 <div className='d-flex mt-3 d-v-center'>
                     <div className='w-20'></div>
                     <div className='w-20'>Password</div>
-                    <input className='w-30 input-1' type="password"/>
+                    <input className='w-30 input-1' type="password"
+                            id="Password_input"
+                            onChange={(event => this.setState({
+                                Password: event.target.value}))}
+                    />
+                    {this.state.WrongInput[2]}
                 </div>
                 <div className='d-flex mt-3 d-v-center'>
                     <div className='w-20'></div>
                     <div className='w-20'>Password Confirm</div>
-                    <input className='w-30 input-1' type="password"/>
+                    <input className='w-30 input-1' type="password"
+                            id="Password_Confirm_input"
+                            onChange={(event => this.setState({
+                                Password_Confirm: event.target.value}))}
+                    />
+                    {this.state.WrongInput[3]}
                 </div>
                 <div className='d-flex mt-3'>
                     <div className='w-20'></div>
-                    <input className='checkbox' type='checkbox'/>
+                    <input className='checkbox' type='checkbox'
+                            id="Term_Use_button"
+                            onChange={(event => this.setState({
+                                Term_of_Use: !this.state.Term_of_Use}))}
+                    />
                     <div className='ml-1'>Terms of Use</div>
+                    {this.state.WrongInput[4]}
                 </div>
                 <div className='d-flex mt-1'>
                     <div className='w-20'></div>
-                    <input className='checkbox' type='checkbox'/>
+                    <input className='checkbox' type='checkbox'
+                            id="Term_Personal_button"
+                            onChange={(event => this.setState({
+                                Term_of_Personal_info: !this.state.Term_of_Personal_info}))}
+                    />
                     <div className='ml-1'>Collect and use of personal information</div>
+                    {this.state.WrongInput[5]}
                 </div>
                 <div className='t-center mt-4 d-flex'>
                     <div className='w-30'></div>
-                    <button className='w-40 button-blue'><Link to='/'>Sign up</Link></button>
+                    <button className='w-40 button-blue'
+                            id="Signup_button"
+                            onClick = {()=>this.clickSignUp()}
+                    >Sign up</button>
                     <div className='w-30'></div>
                 </div>
             </div>
--- fevertime_frontend/src/components/TestCodes/Signup.test.js
+++ fevertime_frontend/src/components/TestCodes/Signup.test.js
@@ -6,11 +6,26 @@ import { getMockStore } from '../../test-utils/mocks';
 
 import { history } from '../../store/store';
 import {ConnectedRouter} from "connected-react-router";
+import axios from "axios"
 
 const mockStore = getMockStore({login : false});
 
+axios.get = jest.fn(url => {
+    return new Promise((resolve, reject) => {
+      const result = {
+        status: 200,
+        data: stubCMs
+      };
+      resolve(result);
+    })})
+
 describe('Signup', () => {
     let signup;
+    let ID = "ValidID"
+    let Nickname = "Nick"
+    let Password = "1234"
+    let Password_C = "1234"
+
     beforeEach(() => {
         signup = (
             <Provider store={mockStore}>
@@ -20,9 +35,144 @@ describe('Signup', () => {
             </Provider>
         );
     })
+    afterEach(() => { jest.clearAllMocks() });
+
     it('should render', () => {
         const component = mount(signup);
         expect(component.find('.Signup').length).toBe(1);
+    });
+
+    it("should take check Valid ID inputs", (done) => {
+        axios.get = jest.fn(url => {
+            return new Promise((resolve, reject) => {
+              const result = {
+                status: 200,
+                data: true
+              };
+              resolve(result);
+            })})
+                
+        const component = mount(signup);
+        const ID_input = component.find('#ID_input');
+        ID_input.simulate('change', { target: { value: ID } });
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[0]).toEqual("");
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        done();
+    }); 
+    
+    it("should take check Invalid ID inputs", (done) => {
+        axios.get = jest.fn(url => {
+            return new Promise((resolve, reject) => {
+              const result = {
+                status: 200,
+                data: false
+              };
+              resolve(result);
+            })})
+                
+        const component = mount(signup);
+        const ID_input = component.find('#ID_input');
+        ID_input.simulate('change', { target: { value: ID } });
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[0]).toEqual("ID Exist");
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        done();
+    });    
+
+    it("should take check ID Deadserver", (done) => {
+        axios.get = jest.fn(url => {
+            return new Promise((resolve, reject) => {
+              const result = {
+                status: 404,
+                data: false
+              };
+              resolve(result);
+            })})
+                
+        const component = mount(signup);
+        const ID_input = component.find('#ID_input');
+        ID_input.simulate('change', { target: { value: ID } });
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[0]).toEqual("Server Not responding");
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        done();
+    });
+
+    it("should take check null Nickname", () => {
+        const component = mount(signup);
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[1]).toEqual("Empty Nickname");
+    }); 
+
+    it("should take check null password", () => {
+        const component = mount(signup);
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[2]).toEqual("Empty Password");
+    });
+
+    it("should take check non-matching password", () => {
+        const component = mount(signup);
+        const PW_input = component.find('#Password_input');
+        PW_input.simulate('change', { target: { value: Password } });
+
+        const PWC_input = component.find('#Password_Confirm_input');
+        PWC_input.simulate('change', { target: { value: "123" } });
+
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[3]).toEqual("Wrong Password Confirm");
+    });
+
+    it("should take term of uses", () => {
+        const component = mount(signup);
+        component.find("#Signup_button").simulate("click");
+        const newSignup = component.find(Signup).instance();
+        expect(newSignup.state.WrongInput[4]).toEqual("Please Check");
+        expect(newSignup.state.WrongInput[5]).toEqual("Please Check");
+    });
+
+    it("should post if correct", (done) => {
+        const component = mount(signup);
+        axios.get = jest.fn(url => {
+            return new Promise((resolve, reject) => {
+              const result = {
+                status: 200,
+                data: true
+              };
+              resolve(result);
+            })})
+
+        axios.post = jest.fn((url, dd) => {
+            return new Promise((resolve, reject) => {
+                const result = {
+                status: 200,
+                data: true
+                };
+                resolve(result);
+            })})
+
+        let spyhistoryPush = jest.spyOn(history, 'push')
+        .mockImplementation(() => { return path => {}; })
+                
+        const ID_input = component.find('#ID_input');
+        ID_input.simulate('change', { target: { value: ID } });
+        component.find('#Nickname_input').simulate('change', { target: { value: Nickname } });
+        component.find('#Password_input').simulate('change', { target: { value: Nickname } });
+        component.find('#Password_Confirm_input').simulate('change', { target: { value: Nickname } });
+        component.find('#Term_Use_button').simulate('change', { target: { value: Nickname } });
+        component.find('#Term_Personal_button').simulate('change', { target: { value: Nickname } });
+
+        component.find("#Signup_button").simulate("click");        
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        expect(axios.post).toHaveBeenCalledTimes(1);
+        expect(spyhistoryPush).toHaveBeenCalledTimes(1);
+        done();
     });
 
 
