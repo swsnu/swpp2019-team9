d5c0c30cfedd4e57803e1f4a95aead617b62204a
inwhanchun <wormfry@naver.com>
2019-10-27 21:44:07 +0900

    backend wo pyc

--- fevertime_backend/user/models.py
+++ fevertime_backend/user/models.py
@@ -2,5 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
+    nickname= models.CharField(max_length=64)
     ToU = models.BooleanField(default=True)
     PI = models.BooleanField(default=True)
--- fevertime_backend/user/views.py
+++ fevertime_backend/user/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest,JsonResponse
 from .models import User
+import json
+from json import JSONDecodeError
+from .models import User
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 # Create your views here.
 
-def signup(request):    
-    return HttpResponse(stauts=200)
+def signup(request):    #copy&paste HW3
+    if request.method == 'POST':
+        try:
+            req_data = json.loads(request.body.decode())
+            username = req_data['username']
+            password = req_data['password']
+            nickname = req_data['nickname']
+        except(KeyError, JSONDecodeError):
+            return HttpResponseBadRequest()         #400
+        User.objects.create_user(username=username, password=password, nickname= nickname)
+        return HttpResponse(status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])     #405
+
+@csrf_exempt
+def signin(request):
+    if request.method =='POST':
+        try:
+            body= request.body.decode()
+            username = json.loads(body)['username']
+            password = json.loads(body)['password']
+        except(KeyError, JSONDecodeError):
+            return HttpResponseBadRequest()         #400
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            res_dict={'id':user.id, 'username':user.username, 'nickname':user.nickname}
+            return JsonResponse(res_dict,status=200)
+        else:
+            return HttpResponse(status=401)
+    else: 
+        return HttpResponseNotAllowed(['POST'])     #405
