9351a6c33f7146e24032267bfdff1cea93a8cce3
inwhanchun <wormfry@naver.com>
2019-10-28 16:28:51 +0900

    10/28 ChunInWhan added mock to friend test

--- fevertime_frontend/src/components/TestCodes/Friends.test.js
+++ fevertime_frontend/src/components/TestCodes/Friends.test.js
@@ -9,6 +9,18 @@ import {ConnectedRouter} from "connected-react-router";
 
 const mockStore = getMockStore({login : false});
 
+jest.mock('../component/PopUpFilled', () => {
+    return jest.fn(props => {
+      return (
+        <div className="spyAddGroupPopup">
+            <button id="spyConfirm" onClick={this.props.clickConfirm}/>
+            <button id="spyClose" onClick={this.props.clickClose}/>
+            <input id='spyContent' onChange={this.props.changeContent}/>
+        </div>
+        );
+    });
+});
+
 describe('Friends', () => {
     let friends;
     beforeEach(() => {
