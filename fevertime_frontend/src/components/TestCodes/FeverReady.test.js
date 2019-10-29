5fd0c10da5393f7425c951ba1d3d65cf1c375546
YooYoungJae <dudwo235@snu.ac.kr>
2019-10-29 19:09:24 +0900

    10/27 (월) 19:19 YooYoungJae test code 추가

--- fevertime_frontend/src/components/TestCodes/FeverReady.test.js
+++ fevertime_frontend/src/components/TestCodes/FeverReady.test.js
@@ -1,34 +1,67 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
-import FeverReady from '../FeverReady';
+import * as FeverReady from '../FeverReady';
 import { getMockStore } from '../../test-utils/mocks';
 
 import { history } from '../../store/store';
 import {ConnectedRouter} from "connected-react-router";
+import * as actionCreator from "../../store/actions/fever";
 
 const mockStore = getMockStore({},{});
 
 describe('FeverReady', () => {
-    let feverready;
+    let feverReady;
+    let spy1 =jest.spyOn(FeverReady.default.WrappedComponent.prototype, 'timerStart');
+    let spy2 =jest.spyOn(FeverReady.default.WrappedComponent.prototype, 'timerAction');
+
+
+    afterEach(() => {
+        spy1.mockClear();
+        spy2.mockClear();
+    })
+
     beforeEach(() => {
-        feverready = (
+        feverReady = (
             <Provider store={mockStore}>
                 <ConnectedRouter history={history}>
-                    <FeverReady/>
+                    <FeverReady.default/>
                 </ConnectedRouter>
             </Provider>
         );
     })
     it('should render', () => {
+        const spypostFeverHistory = jest.spyOn(actionCreator, 'postFeverHistory')
+            .mockImplementation((cat,etcCat) => { return dispatch => {}; });
+        jest.useFakeTimers();
         const originalError = console.error;
         const error = jest.fn()
         console.error = error;
-        const component = mount(feverready);
+        const component = mount(feverReady);
         expect(component.find('.FeverReady').length).toBe(1);
         expect(error).toHaveBeenCalledWith("Warning: unstable_flushDiscreteUpdates: Cannot flush updates when React is already rendering.%s", expect.any(String));
         console.error = originalError;
+
+        jest.advanceTimersByTime(2000);
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalled();
+        jest.advanceTimersByTime(10000);
+
+        expect(spypostFeverHistory).toHaveBeenCalled();
+
+        // const componentWillUnmount =jest.spyOn(FeverReady.default.WrappedComponent.prototype, 'componentWillUnmount');
+        // component.unmount();
+        // expect(componentWillUnmount).toHaveBeenCalled();
+
     });
 
+    it(`should call 'putFeverHistory'`, () => {
+        const spypostFeverHistory = jest.spyOn(actionCreator, 'postFeverHistory')
+            .mockImplementation(hid => { return dispatch => {}; });
+        const component = mount(feverReady);
+        const wrapper = component.find('#fever-ready-click-skip');
+        wrapper.simulate('click');
+        expect(spypostFeverHistory).toHaveBeenCalled();
+    });
 
 });
