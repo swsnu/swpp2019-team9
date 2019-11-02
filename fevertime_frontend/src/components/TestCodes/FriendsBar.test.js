7f1601bb94419ddafb0d60b96f50f1d2dd6fed23
inwhanchun <wormfry@naver.com>
2019-11-02 12:47:38 +0900

    11/2 inwhanchun made frontend test

--- fevertime_frontend/src/components/TestCodes/FriendsBar.test.js
+++ fevertime_frontend/src/components/TestCodes/FriendsBar.test.js
@@ -3,15 +3,39 @@ import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import FriendsBar from '../component/FriendsBar';
 import { getMockStore } from '../../test-utils/mocks';
-
+import axios from 'axios';
+import PopupFilled from '../component/PopupFilled'
 import { history } from '../../store/store';
 import {ConnectedRouter} from "connected-react-router";
 
+
+jest.mock('../component/PopupFilled', () => {
+    return jest.fn(props => {
+      return (
+        <div className="spyAddFriendPopup">
+            <button id="spyConfirm" onClick={props.clickConfirm}/>
+            <button id="spyClose" onClick={props.clickClose}/>
+            <input id='spyContent' onChange={props.changeContent}/>
+        </div>
+        );
+    });
+});
 const stubInitialUser = {
     uid:1,
     username: 'test',
     nickname:'test',
 }
+
+axios.get = jest.fn(() => {
+    return new Promise((resolve) => {
+        const result = {
+            status: 200,
+            data: [{'nickname': 'test'}]
+        };
+        resolve(result);
+    })
+});
+
 const mockStore = getMockStore(stubInitialUser,{});
 describe('FriendsBar', () => {
     let friendsBar;
@@ -27,5 +51,141 @@ describe('FriendsBar', () => {
     it('should render', () => {
         const component = mount(friendsBar);
         expect(component.find('.FriendsBar').length).toBe(1);
-    }); 
+    });
+    it('should accept request',()=>{
+        axios.post = jest.fn(() => {
+            return new Promise((resolve,reject) => {
+                const result = {
+                    status: 201,
+                    data: {'nickname': 'test'}
+                };
+                resolve(result);
+                reject({status:404})
+            })
+        });
+        const component = mount(friendsBar)
+        const button = component.find("#request-tab")
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        FriendsBarInstance.setState({friendlist: [{'firstword' : 'r', 'name': 'real'}],
+                                    friendinglist: [{'firstword' : 'r', 'name': 'request'}]})
+        button.simulate('click')
+        const wrapper1 = component.find("#accept-button")
+        wrapper1.simulate('click')
+        expect(axios.post).toHaveBeenCalledTimes(1);
+    })
+    it('should delete friend',()=>{
+        axios.delete = jest.fn(() => {
+            return new Promise((resolve) => {
+                const result = {
+                    status: 200
+                };
+                resolve(result);
+            })
+        });
+        const component = mount(friendsBar)
+        const button = component.find("#real-tab")
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        FriendsBarInstance.setState({friendlist: [{'firstword' : 'r', 'name': 'real'}],
+                                    friendinglist: [{'firstword' : 'r', 'name': 'request'}]})
+        button.simulate('click')
+        const wrapper2 = component.find("#delete-button")
+        wrapper2.simulate('click')
+        expect(axios.delete).toHaveBeenCalledTimes(1);
+    })
+    it('should decline request',()=>{
+        axios.delete = jest.fn(() => {
+            return new Promise((resolve) => {
+                const result = {
+                    status: 200
+                };
+                resolve(result);
+            })
+        });
+        const component = mount(friendsBar)
+        const button = component.find("#request-tab")
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        FriendsBarInstance.setState({friendlist: [{'firstword' : 'r', 'name': 'real'}],
+                                    friendinglist: [{'firstword' : 'r', 'name': 'request'}]})
+        button.simulate('click')
+        const wrapper2 = component.find("#decline-button")
+        wrapper2.simulate('click')
+        expect(axios.delete).toHaveBeenCalledTimes(1);
+    })
+    it('should close send',()=>{
+        axios.post = jest.fn(() => {
+            return new Promise((resolve,reject) => {
+                const result = {
+                    status: 201,
+                    data: {'nickname': 'test'}
+                };
+                resolve(result);
+                reject({status:404})
+            })
+        });
+        const component = mount(friendsBar)
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        const button = component.find("#add-friend-button")
+        button.simulate('click')
+        const wrapper1 = component.find("#spyClose")
+        wrapper1.simulate('click')
+        expect(axios.post).toHaveBeenCalledTimes(0);
+    })
+    it('should send request',()=>{
+        axios.post = jest.fn(() => {
+            return new Promise((resolve) => {
+                const result = {
+                    status: 201,
+                    data: {'nickname': 'test'}
+                };
+                resolve(result);
+            })
+        });
+        const nickname='test'
+        const component = mount(friendsBar)
+        const button = component.find("#add-friend-button")
+        button.simulate('click')
+        const wrapper = component.find("#spyContent")
+        wrapper.simulate('change', {target:{value:nickname}});
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        expect(FriendsBarInstance.state.friendname).toEqual('test')
+        const wrapper1 = component.find("#spyConfirm")
+        wrapper1.simulate('click')
+        expect(axios.post).toHaveBeenCalledTimes(1);
+    })
+    it('should give alert',()=>{
+        global.alert=jest.fn();
+        const component = mount(friendsBar)
+        const button = component.find("#add-friend-button")
+        button.simulate('click')
+        const wrapper1 = component.find("#spyConfirm")
+        wrapper1.simulate('click')
+        expect(global.alert).toHaveBeenCalledTimes(1);
+    })
+    /*
+    it('should give reject',()=>{
+        global.alert=jest.fn();
+        axios.post = jest.fn(() => {
+            return new Promise((resolve,reject) => {
+                const result = {
+                    status: 404,
+                    data: null
+                };
+                resolve()
+                reject(new Error(result))
+            })
+        });
+        const nickname='test'
+        const component = mount(friendsBar)
+        const button = component.find("#add-friend-button")
+        button.simulate('click')
+        const wrapper = component.find("#spyContent")
+        wrapper.simulate('change', {target:{value:nickname}});
+        const FriendsBarInstance = component.find(FriendsBar).instance();
+        expect(FriendsBarInstance.state.friendname).toEqual('test')
+        const wrapper1 = component.find("#spyConfirm")
+        wrapper1.simulate('click')
+        expect(global.alert).toHaveBeenCalledTimes(1);
+    })
+    */
+   
 });
--- fevertime_frontend/src/components/component/FriendsBar.js
+++ fevertime_frontend/src/components/component/FriendsBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import AddFriendPopup from "./PopupFilled";
-import PropTypes from 'prop-types';
 class FriendsBar extends Component {
     constructor (props)
     {
@@ -73,13 +72,16 @@ class FriendsBar extends Component {
         }
         else{
             axios.post('/api/friend/request/',{'nickname':this.state.friendname})
-                .then(this.setState({
+                .then(()=>{
+                    this.setState({
                     showAddFriendPopup : false,
                     friendname : '',
-                }))
-                .catch(
-                    //alert('no such name!')  have to define
-                    )
+                    })
+                })
+                .catch(error=>{
+                    if(error.response.status===404)
+                        alert('no such name!')
+                })
         }
     }
     clickDeleteReal= (name)=>{
@@ -98,8 +100,8 @@ class FriendsBar extends Component {
                             changeContent={this.friendNameChange}
                 />
                 <div className='d-flex fri-list-button'>
-                    <div className={(this.state.showMyFriend ? 'show-my-friend-tab' : 'hide-my-friend-tab')} onClick={this.clickMyFriends()}>My Friends</div>
-                    <div className={(!this.state.showMyFriend ? 'show-my-friend-tab' : 'hide-my-friend-tab')} onClick={this.clickFriendingList()}>Friending list</div>
+                    <div id='real-tab' className={(this.state.showMyFriend ? 'show-my-friend-tab' : 'hide-my-friend-tab')} onClick={this.clickMyFriends()}>My Friends</div>
+                    <div id='request-tab' className={(!this.state.showMyFriend ? 'show-my-friend-tab' : 'hide-my-friend-tab')} onClick={this.clickFriendingList()}>Friending list</div>
                 </div>
                 <div className='pl-3 friend-scroll'>
                     {this.state.showMyFriend ? (
@@ -109,7 +111,7 @@ class FriendsBar extends Component {
                                         <div className='d-flex mt-2' key={index}>
                                             <div className='badge-custom t-center'>{value.firstword}</div>
                                             {value.name}
-                                            <button onClick={()=>this.clickDeleteReal(value.name)}>Delete</button>
+                                            <button onClick={()=>this.clickDeleteReal(value.name)} id='delete-button'>Delete</button>
                                         </div>
                                     );
                                 })}
@@ -121,8 +123,8 @@ class FriendsBar extends Component {
                                     <div className='d-flex mt-2' key={index}>
                                         <div className='badge-custom t-center'>{value.firstword}</div>
                                         {value.name}
-                                        <button onClick={()=>this.clickAcceptRequest(value.name)}>O</button>
-                                        <button onClick={()=>this.clickDeclineRequest(value.name)}>X</button>
+                                        <button onClick={()=>this.clickAcceptRequest(value.name)} id='accept-button'>O</button>
+                                        <button onClick={()=>this.clickDeclineRequest(value.name)} id='decline-button'>X</button>
                                     </div>
                                 );
                             })}
@@ -130,7 +132,7 @@ class FriendsBar extends Component {
 
                 </div>
                 <div className='add-friend-button-wrapper'>
-                    <button className='mt-3 w-100 button-blue' onClick={this.clickAddFriend}>Add friend</button>
+                    <button id='add-friend-button' className='mt-3 w-100 button-blue' onClick={this.clickAddFriend}>Add friend</button>
                 </div>
             </div>
         )
