14dca6dcfdc05a6860fdbdbed4d0da0bc7511155
manhair <leesg9@snu.ac.kr>
2019-11-11 18:11:39 +0900

    11/11 mysql in local. check inst. in notion

--- fevertime_backend/fevertime/settings.py
+++ fevertime_backend/fevertime/settings.py
@@ -83,8 +83,15 @@ WSGI_APPLICATION = 'fevertime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        #'OPTIONS': {
+        #    'read_default_file': '/mysql.cnf',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'fevertime',
+        'USER': 'user',
+        'PASSWORD': 'fever1234',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3000',
     }
 }
 
