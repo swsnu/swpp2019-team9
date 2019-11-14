5a815070056801d760a4c6ee4f459514620a4a63
manhair <leesg9@snu.ac.kr>
2019-11-14 17:54:41 +0900

    11/14 keep showing data from url

--- fevertime_frontend/src/components/FeverEnd.js
+++ fevertime_frontend/src/components/FeverEnd.js
@@ -4,28 +4,46 @@ import {connect} from 'react-redux'
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 class FeverEnd extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            total_time : "",
+            fever_time : "",
+            fever_rate : "",
+        }
+    }
+    componentDidMount(){
+        var search = this.props.location.search
+        var params = new URLSearchParams(search)
+        console.log(params.get('total_time'))
+        var params
+        this.setState({
+            total_time : params.get('total_time'),  
+            fever_time : params.get('fever_time'),
+            fever_rate : params.get('fever_rate'),
+    })
+}
     render() {
         return (
             <div className='form-container FeverEnd'>
                 <div className='d-flex d-v-center mt-5'>
                     <div className='w-30  page-title'>Fever mode</div>
-
                 </div>
                 <div className=' mt-5 d-v-center  fever-form'>
                     <div className='d-flex mt-5'>
                         <div className='w-20'></div>
                         <div className='w-40 f-large'>Total Time </div>
-                        <div className='w-20 f-xlarge '>{this.props.total_time}</div>
+                        <div className='w-20 f-xlarge '>{this.state.total_time}</div>
                     </div>
                     <div className='d-flex mt-5'>
                         <div className='w-20'></div>
                         <div className='w-40 f-large'>Fever Time </div>
-                        <div className='w-20 f-xlarge '>{this.props.fever_time}</div>
+                        <div className='w-20 f-xlarge '>{this.state.fever_time}</div>
                     </div>
                     <div className='d-flex mt-5'>
                         <div className='w-20'></div>
                         <div className='w-40 f-large'>Avg Fever rate </div>
-                        <div className='w-20 f-xlarge '> {this.props.fever_rate}%</div>
+                        <div className='w-20 f-xlarge '> {this.state.fever_rate}%</div>
                     </div>
                 </div>
                 <div className='d-flex d-ho-center'>
