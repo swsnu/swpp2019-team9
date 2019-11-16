9eb6942b88daa3ec7a3c87dd183ab6772245dec3
inwhanchun <wormfry@naver.com>
2019-11-16 21:11:52 +0900

    mysql in desktop works. test?

--- fevertime_backend/fevertime/settings.py
+++ fevertime_backend/fevertime/settings.py
@@ -84,8 +84,13 @@ WSGI_APPLICATION = 'fevertime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': "./my.cnf",
+            #'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
