7fe2c952dfaf483106a09a9154dd727048949558
YooYoungJae <dudwo235@snu.ac.kr>
2019-11-20 03:11:48 +0900

    11/20(수) 02:51 Youngjae django testcode 추가

--- fevertime_backend/fever/tests.py
+++ fevertime_backend/fever/tests.py
@@ -8,6 +8,12 @@ class FeverTestCase(TestCase):
         User.objects.create_user(username="youngjae",
                                  password="youngjae",
                                  nickname="youngjae")
+        User.objects.create_user(username="youngjae2",
+                                 password="youngjae2",
+                                 nickname="youngjae2")
+        User.objects.create_user(username="youngjae3",
+                                 password="youngjae3",
+                                 nickname="youngjae3")
         # user2 = User.objects.create_user(username="youngjae2",
         #                                  password="youngjae2",
         #                                  nickname="youngjae2")
@@ -181,3 +187,25 @@ class FeverTestCase(TestCase):
         # # #405 테스트 too-many-branch 때매 지움
         # response = client.delete('/api/fever_exception/')
         # self.assertEqual(response.status_code, 405)
+
+    def test_ffever_top_list(self):
+        client = Client(enforce_csrf_checks=False)
+
+        response = client.post('/api/user/signin/',
+                               json.dumps({'username': 'youngjae', 'password': 'youngjae'}),
+                               content_type='application/json')
+
+
+        # POST #200 테스트
+        response = client.post('/api/fever_history/',
+                               json.dumps({'category': 'Study', 'etcCategory': '',
+                                           'goalTime': '02:00'}),
+                               content_type='application/json')
+
+
+        response = client.put('/api/fever_history/', json.dumps({'id': '1'}),
+                              content_type='application/json')
+
+
+        response = client.get('/api/fever_top_list/')
+        self.assertEqual(response.status_code, 200)
