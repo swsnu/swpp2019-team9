e4cb1438f32280f0a8c2139bde656bb856d10520
YooYoungJae <dudwo235@snu.ac.kr>
2019-11-26 01:37:23 +0900

    11/26(화) 01:37 유영재 token 얻는 api 추가

--- fevertime_backend/user/tests.py
+++ fevertime_backend/user/tests.py
@@ -194,3 +194,24 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 401)
 
         #others in friend, group test
+
+    def test_csrf(self):
+        # By default, csrf checks are disabled in test client
+        # To test csrf protection we enforce csrf checks here
+        client = Client(enforce_csrf_checks=True)
+
+
+        response = client.post('/api/user/signup/', json.dumps(
+            {'username': 'youngjae', "nickname": "youngjae", 'password': 'youngjae', "wrong": False}),
+                            content_type='application/json')
+        self.assertEqual(response.status_code, 403)  # Request without csrf token returns 403 response
+
+        response = client.get('/api/user/token/')
+        csrftoken = response.cookies['csrftoken'].value  # Get csrf token from cookie
+
+        response = client.post('/api/user/signup/', json.dumps(
+            {'username': 'youngjae', "nickname": "youngjae", 'password': 'youngjae', "wrong": False}),
+                               content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
+        self.assertEqual(response.status_code, 201)  # Pass csrf protection
+
+
--- fevertime_backend/user/urls.py
+++ fevertime_backend/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signout/', views.signout,name='signout'),
     path('social/', views.social,name='social'),
     path('social/<int:user_id>/', views.social_specific, name='social_specific'),
+    path('token/', views.token,name='token'),
     path('', views.user, name='default'),
 ]
